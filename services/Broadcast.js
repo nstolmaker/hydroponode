eb1bdd5360f04ee588cda32229b18b76df47e8c3
Noah Stolmaker <nstolmaker@gmail.com>
2021-08-31 13:57:57 -0700

    put in the stuff to complete the task, but lets see if it runs

--- services/Broadcast.js
+++ services/Broadcast.js
@@ -11,8 +11,11 @@ export class Broadcast {
   }
   broadcastToWorkflowEngine(sensorData) {
     console.log("Broadcasting to workflow engine, sending sensorData: ", sensorData)
-    this.sensorData = sensorData
-   fetchAndLockOneTask()
+    // this.sensorData = sensorData
+    const taskUnit = fetchAndLockOneTask()
+    console.log("taskUnit returned from server is below. look for an id and pass it into the completed function", taskUnit)
+    const taskId = taskUnit.id
+    this.sendSensorData(taskId, sensorData)
   }
   /**
    * Before we can do anything to a task in camunda, we have to take ownership of it and give it a temporary lock.
@@ -44,8 +47,61 @@ export class Broadcast {
     // response
     if (response.status === '204') {
       console.log("Fetched and locked one in! Go ahead and send in the data now.")
+      console.log("Server response looks like this: ",response)
+      return response.body
     } else {
       console.log("Fetched and locked but unrecognized status: ", response.status, response.statusText)
+      return false
+    }
+  }
+
+  /**
+   * @param {moisture, light, temperature, battery_level} sensorData 
+   * @description closes the workflow external task that's waiting, by sending it actual sensor data
+   */
+  async sendSensorData(taskId, sensorData) {
+    // payload
+    const bodyPayload = {
+      "workerId": "tuesdayWorker",
+      "variables": {
+        "moisture": {
+          "value": sensorData.moisture,
+          "type": "Integer"
+        },
+        "light": {
+          "value": sensorData.light,
+          "type": "Integer"
+        },
+        "temperature": {
+          "value": sensorData.temperature,
+          "type": "Integer"
+        },
+        "battery": {
+          "value": sensorData.battery_level,
+          "type": "Integer"
+        }
+      },
+    }
+
+    const endpointPath = `${this.workflowEngineAddress}/engine-rest/external-task/${taskId}/complete`
+    
+
+    // request
+    const response = await fetch(endpointPath, {
+      body: JSON.stringify(bodyPayload),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }  
+    })
+
+    // response
+    if (response.status === '204') {
+      console.log("Sensor Data sent!")
+      return true
+    } else {
+      console.log("Sensor Data sent but unrecognized status: ", response.status, response.statusText)
+      return false
     }
   }
 }
