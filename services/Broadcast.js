742841ffe985de48cc81a1a07dd5b9e99c52a932
Noah Stolmaker <nstolmaker@gmail.com>
2021-12-17 22:59:43 -0800

    this has gotta fix it too

--- services/Broadcast.js
+++ services/Broadcast.js
@@ -129,10 +129,7 @@ export class Broadcast {
 
   async recordSensorDataInDb(sensorData) {
     console.log("About to send sensor data to endpoint: ", this.databaseEndpoint + 'sensor-data');
-    const bodyPayload = 
-    {
-      data: sensorData
-    }
+    const bodyPayload = sensorData
     
 	  console.log("Payload is: ", bodyPayload);
     const response = await axios({
