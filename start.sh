f1cc3ad6dd052aeb9c8929690af2eba651fe6b1d
Noah Stolmaker @ Pi <nstolmaker@gmail.com>
2023-03-08 10:32:39 -0800

    changing this to every 10 minutes aand reducing the restart delay to 30 seconds. the delay only happens if the process dies. if it completes normally i THINK it should just let it turn off. maybe not though. anyway i think the restart delay was what was causing the processs to seem to double and then tripple up over time. the failures would basically get restarted exactly when the next one was going to start. theres an option that will only restart on certain error codes, but it doesnt seem to be implemented yet. i found the PR but it wasnt merged as of last year. if it ever gets smerged then maybe use that insead of this restart delay because i think that would be better. ALSO changing the cron timing to be every 10 minutess instead of 5

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pm2 start "node -- app.js" --cron "1,6,11,16,21,26,31,36,41,46,51,56 * * * *" --restart-delay 300000
+pm2 start "node -- app.js" --cron "1,11,21,31,41,51 * * * *" --restart-delay 30000
